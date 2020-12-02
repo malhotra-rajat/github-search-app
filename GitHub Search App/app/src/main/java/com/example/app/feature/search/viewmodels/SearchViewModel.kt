26928d95edd347ea6741bc57b552f0bb59fd51f8
Desktop <malhotra.rajat1988@gmail.com>
2020-12-02 15:02:49 -0500

    Expose getter for reposResult

--- GitHub Search App/app/src/main/java/com/example/app/feature/search/viewmodels/SearchViewModel.kt
+++ GitHub Search App/app/src/main/java/com/example/app/feature/search/viewmodels/SearchViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.app.feature.search.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -12,13 +13,17 @@ import kotlinx.coroutines.launch
 
 class SearchViewModel @ViewModelInject constructor(private val githubRepository: GithubRepository): ViewModel() {
 
-    var reposResult = MutableLiveData<Result<GithubReposModel>>()
+    private val _reposResult = MutableLiveData<Result<GithubReposModel>>()
+
+    val reposResult: LiveData<Result<GithubReposModel>>
+            get() = _reposResult
+
     var dispatcher = Dispatchers.IO
 
     fun searchRepos(org: String) {
         viewModelScope.launch (dispatcher) {
             val reposResultModel = githubRepository.searchRepos(org)
-            reposResult.postValue(reposResultModel)
+            _reposResult.postValue(reposResultModel)
         }
     }
 }
